d9b51f6d2e11eec0e33ea8b5db937b94f6e96bd8
Vincent <vincent14@student.umn.ac.id>
2023-03-07 13:05:43 +0700

    adding form and settng tailwind

---
+++ resources/views/form.blade.php
@@ -0,0 +1,7 @@
+<div class="mb-6">
+  <form class="flex flex-col space-y-4" method="post" action="/">
+    <input type="text" name="title" placeholder="Todo title" class="py-3 px-4 bg-gray-100 rounded-xl">
+    <textarea name="description" placeholder="Todo description" class="py-3 px-4 bg-gray-100 rounded-xl"></textarea>
+    <button class="w-28 py-3 px-8 bg-green-500 text-white rounded-xl">Add</button>
+  </form>
+</div>
