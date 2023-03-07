ad819aff650ee268d54a4f3c96f2d561232d6828
Vincent <vincent14@student.umn.ac.id>
2023-03-07 13:24:28 +0700

    adding item ui

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,23 +1,21 @@
 <!doctype html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
+
 <body class="bg-gray-200 p-4">
   <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl">
-    <h1 class="font-bold text-5xl text-center mb-8">ToDo</h1>
-
+    <h1 class="font-bold text-5xl text-center mb-8">ToDo Laravel</h1>
     @include('form')
-
     <hr>
-
     <div class="mt-2">
-      <div class="py-4 flex items-center border-b border-gray-300 px-3">
-
-      </div>
+      @include('item')
     </div>
   </div>
 </body>
+
 </html>
